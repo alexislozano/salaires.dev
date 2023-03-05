4ebeba1b6dd443a18c5ef9077e09cda7604541f1
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2023-03-06 00:41:37 +0100

    only keep fetch api

--- back/src/app/api/mod.rs
+++ back/src/app/api/mod.rs
@@ -1,13 +1,9 @@
-mod confirm_token;
 mod fetch_companies;
 mod fetch_locations;
 mod fetch_salaries;
 mod fetch_titles;
-mod insert_salary;
 
-pub use confirm_token::confirm_token;
 pub use fetch_companies::fetch_companies;
 pub use fetch_locations::fetch_locations;
 pub use fetch_salaries::fetch_salaries;
 pub use fetch_titles::fetch_titles;
-pub use insert_salary::insert_salary;
